5d031642f8fcf19dae4c53ef6600f83814da591b
Kkim1119 <kibumkim1119@gmail.com>
2022-07-30 00:59:39 -0700

    creates room with robot and obstacle

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-print("Hello World")
+from tkinter import *
+import time
+
+def drag_robot(event):
+    room.move(robot,event.x,event.y)
+    room.update()
+    print(event.x,event.y)
+
+window = Tk()
+
+
+
+room = Canvas(window, height= 600, width=600)
+room.pack()
+
+robot_image = PhotoImage(file="robot.png")
+robot_image = robot_image.zoom(5) #Makes the image 5 times bigger
+robot_image = robot_image.subsample(32) #shrinks down the image(higher number -> gets smaller
+robot = room.create_image(500,500, image=robot_image)
+
+
+
+room.create_rectangle(100, 100, 250, 250, fill="Black")
+
+
+window.mainloop()
 
 
