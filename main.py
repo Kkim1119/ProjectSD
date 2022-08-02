f9e8c4abc055042c9acc9d637aeef01253fb5b58
Kkim1119 <kibumkim1119@gmail.com>
2022-08-02 00:04:25 -0700

    tested test_list

--- main.py
+++ main.py
@@ -4,33 +4,38 @@ from math import *
 from PIL import Image, ImageDraw
 
 #---------------------------------------------
-def scan(img):
+def scan(robot_x, robot_y, img):
     degree = 0
     pixels = 1
     ten_rad = 10 * (pi/180)
-
+    coord_list = []
     px = img.load()
-    print(px [ROBOT_X+1, ROBOT_Y+1])
+    print(px [robot_x+1, robot_y+1])
 
     while not(degree <= -(2*pi-ten_rad)):
-        init_x = int((pixels * cos(degree))) + ROBOT_X
-        init_y = int((pixels * sin(degree))) + ROBOT_Y
+        init_x = int((pixels * cos(degree))) + robot_x
+        init_y = int((pixels * sin(degree))) + robot_y
         px[init_x,init_y] = (0,0,255)
-        init2_x = int(((pixels + 1) * cos(degree))) + ROBOT_X
-        init2_y = int(((pixels + 1) * sin(degree))) + ROBOT_Y
+        init2_x = int(((pixels + 1) * cos(degree))) + robot_x
+        init2_y = int(((pixels + 1) * sin(degree))) + robot_y
 
         next_pixel = px[init2_x,init2_y]
         if(next_pixel[0]==0 and next_pixel[1]==0 and next_pixel[2]==0):
             degree -= ten_rad
             pixels = 1
+            coord_list.append([init2_x,init2_y])
         else:
             pixels += 1
-            init_x -= ROBOT_X
-            init_y -= ROBOT_Y
-            init2_x -= ROBOT_X
-            init2_y -= ROBOT_Y
+            init_x -= robot_x
+            init_y -= robot_y
+            init2_x -= robot_x
+            init2_y -= robot_y
+
+    return coord_list
 
 #------------------------------------------------
+
+
 w, h = 1000, 1000
 room = [(100, 100), (700, 700)]
 object = [(150, 150), (300, 400)]
@@ -56,7 +61,11 @@ img3.rectangle(robot, fill="red")
 
 px[ROBOT_X,ROBOT_Y] = (0,0,255)
 
-scan(img)
+
+
+print(test_list := scan(ROBOT_X,ROBOT_Y,img))
+for i in test_list:
+    px[i[0],i[1]]= (255,0,0)
 
 img.show()
 
