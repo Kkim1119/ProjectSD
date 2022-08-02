cfe466d6d1975d0dad6e77d22f2c7d2563fb5902
Kkim1119 <kibumkim1119@gmail.com>
2022-08-02 00:27:35 -0700

    made tracing of scanned points

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ def scan(robot_x, robot_y, img):
     pixels = 1
     ten_rad = 10 * (pi/180)
     coord_list = []
-    px = img.load()
-    print(px [robot_x+1, robot_y+1])
+    scan_px = img.load()
+    print(scan_px [robot_x+1, robot_y+1])
 
     while not(degree <= -(2*pi-ten_rad)):
         init_x = int((pixels * cos(degree))) + robot_x
         init_y = int((pixels * sin(degree))) + robot_y
-        px[init_x,init_y] = (0,0,255)
+        scan_px[init_x,init_y] = (0,0,255)
         init2_x = int(((pixels + 1) * cos(degree))) + robot_x
         init2_y = int(((pixels + 1) * sin(degree))) + robot_y
 
-        next_pixel = px[init2_x,init2_y]
+        next_pixel = scan_px[init2_x,init2_y]
         if(next_pixel[0]==0 and next_pixel[1]==0 and next_pixel[2]==0):
             degree -= ten_rad
             pixels = 1
@@ -34,7 +34,16 @@ def scan(robot_x, robot_y, img):
     return coord_list
 
 #------------------------------------------------
-
+def draw_points(list, img):
+    dp_px = img.load()
+    for i in range(len(test_list)-1):
+        dp_px[test_list[i][0], test_list[i][1]] = (255, 0, 0)
+        connect = ImageDraw.Draw(img)
+        dots = [(test_list[i][0], test_list[i][1]), (test_list[i+1][0], test_list[i+1][1])]
+        connect.line(dots, fill="red")
+        if(i == len(test_list)-2):
+            final_dots = [(test_list[0][0], test_list[0][1]), (test_list[len(test_list)-1][0], test_list[len(test_list)-1][1])]
+            connect.line(final_dots, fill="red")
 
 w, h = 1000, 1000
 room = [(100, 100), (700, 700)]
@@ -45,29 +54,29 @@ ROBOT_X = 500
 ROBOT_Y = 500
 
 # creating new Image object
-img = Image.new("RGB", (w, h), color="white")
-px = img.load()
+main_img = Image.new("RGB", (w, h), color="white")
+px = main_img.load()
 
+points_img = Image.new("RGB", (w, h), color="white")
 # create line image
-img1 = ImageDraw.Draw(img)  #Draws the room border(img1 -> room border)
+img1 = ImageDraw.Draw(main_img)  #Draws the room border(img1 -> room border)
 img1.rectangle(room, outline="#000000", width=7)
 
 
-img2 = ImageDraw.Draw(img)
+img2 = ImageDraw.Draw(main_img)
 img2.rectangle(object, fill="#000000")
 
-img3 = ImageDraw.Draw(img)
+img3 = ImageDraw.Draw(main_img)
 img3.rectangle(robot, fill="red")
 
 px[ROBOT_X,ROBOT_Y] = (0,0,255)
 
 
 
-print(test_list := scan(ROBOT_X,ROBOT_Y,img))
-for i in test_list:
-    px[i[0],i[1]]= (255,0,0)
-
-img.show()
+print(test_list := scan(ROBOT_X,ROBOT_Y,main_img))
+draw_points(test_list, points_img)
+main_img.show()
+points_img.show()
 
 #time.sleep(5)
 #img.show()
